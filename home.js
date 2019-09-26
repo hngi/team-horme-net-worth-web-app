8e47a8a2b3e210d2a70646721af8a3125c282a93
Chidiebere <onyekachisamuel18@gmail.com>
2019-09-26 16:42:39 +0100

    Responsive design

--- home.js
+++ home.js
@@ -6,7 +6,7 @@ let suBtn = document.querySelector('#su')
 let chngTlg = () => {
     login.style.display ='block';
     signUp.style.display='none'
-    suBtn.style.background ='rgba(10, 163, 125, 0.904)';
+    suBtn.style.background ='#1AB188';
     lgBtn.style.background='rgba(32, 43, 44)';
 };
 
@@ -14,8 +14,24 @@ let chngTsu = () => {
     login.style.display ='none';
     signUp.style.display='block';
     suBtn.style.background ='rgb(32, 43, 44)';
-    lgBtn.style.background='rgba(10, 163, 125, 0.904)';
+    lgBtn.style.background='#1AB188';
 };
 
 lgBtn.addEventListener('click',chngTlg)
 suBtn.addEventListener('click',chngTsu)
+
+let open =() => {
+    let menulists = document.querySelectorAll(".part");
+    menulists.forEach(menulist => {
+        menulist.classList.toggle("appear");
+    })
+    // document.querySelector('.part').style.display='block'
+}
+let close =() => {
+    let menulists = document.querySelectorAll(".info");
+    menulists.forEach(menulist => {
+        menulist.classList.toggle("hide");
+    })
+}
+    document.querySelector('.p').addEventListener('click', open);
+    document.querySelector('.p').addEventListener('click', close);
