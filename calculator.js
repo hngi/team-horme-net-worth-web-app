5389fd098c495b256a48b77ef9ba09f0f3ea4d31
samuel-chris <prosam001@gmail.com>
2019-09-27 02:24:01 +0100

    Javascript for calculator

---
+++ calculator.js
@@ -0,0 +1,132 @@
+var assetForm = document.querySelector("#asset-form");
+var liabilityForm = document.querySelector("#liability-form");
+var totalAsset = document.getElementById("total-asset");
+var totalLiability = document.getElementById("total-liability");
+
+
+assetForm.onsubmit = function(e) {
+    e.preventDefault();
+    newAssetField();
+}
+
+liabilityForm.onsubmit = function(e) {
+    e.preventDefault();
+    newLiabilityField();
+}
+
+// function clear() {
+//     document.
+// }
+
+function newAssetField() {
+    var assetForm = document.querySelector("#asset-form");
+    var formLength = assetForm.firstElementChild.children.length/2;
+
+    var asset = document.createElement("div");
+    asset.classList.add("col-md-6");
+    asset.innerHTML = `
+        <div class="form-group m-2">
+            <input
+                type="text"
+                name="value"
+                id="asset${formLength}"
+                placeholder="Asset${formLength + 1}"
+                class="p-4 form-control form-control-lg pl-5"
+            />
+        </div>
+    `
+
+    var assetValue = document.createElement("div");
+    assetValue.classList.add("col-md-6");
+    assetValue.innerHTML = `
+        <div class="form-group m-2">
+            <input
+                type="number"
+                name="value"
+                id="asset-value${formLength}"
+                placeholder=" ASSET ${formLength + 1} monetary worth"
+                onchange="updateTotalAsset()"
+                class="p-4 asset-value form-control form-control-lg pl-5"
+                required
+            />
+        </div>
+    `
+
+    assetForm.firstElementChild.appendChild(asset);
+    assetForm.firstElementChild.appendChild(assetValue);
+}
+
+function newLiabilityField() {
+    var liabilityForm = document.querySelector("#liability-form");
+    var formLength = liabilityForm.firstElementChild.children.length/2;
+    
+    var liability = document.createElement("div");
+    liability.classList.add("col-md-6");
+    liability.innerHTML = `
+        <div class="form-group m-2">
+            <input
+                type="text"
+                name="value"
+                id="liability${formLength}"
+                placeholder="Liability${formLength + 1}"
+                class="p-4 form-control form-control-lg pl-5"
+            />
+        </div>
+    `
+
+    var liabilityValue = document.createElement("div");
+    liabilityValue.classList.add("col-md-6");
+    liabilityValue.innerHTML = `
+        <div class="form-group m-2">
+            <input
+                type="number"
+                name="value"
+                id="liability-value${formLength}"
+                placeholder=" LIABILITY ${formLength + 1} monetary worth"
+                onchange="updateTotalLiability()"
+                class="p-4 asset-value liability-value form-control form-control-lg pl-5"
+                required
+            />
+        </div>
+    `
+
+    liabilityForm.firstElementChild.appendChild(liability);
+    liabilityForm.firstElementChild.appendChild(liabilityValue);
+}
+
+function updateTotalAsset() {
+    var totalAsset = document.getElementById("total-asset");
+    var assetValues = document.getElementsByClassName("asset-value");
+    var total = 0;
+    for (i=0; i < assetValues.length; i++) { 
+        if (assetValues[i].value !== "") {
+            total += parseFloat(assetValues[i].value);
+        }
+    }
+    totalAsset.innerHTML = total;
+
+    updateNetWorth();
+}
+
+function updateTotalLiability() {
+    var totalLiability = document.getElementById("total-liability");
+    var liabilityValues = document.getElementsByClassName("liability-value");
+    var total = 0;
+    for (i=0; i < liabilityValues.length; i++) { 
+        if (liabilityValues[i].value !== "") {
+            total += parseFloat(liabilityValues[i].value);
+        }
+    }
+    totalLiability.innerHTML = total;
+
+    updateNetWorth();
+}
+
+function updateNetWorth() {
+    var assets = totalAsset.innerHTML || 0;
+    var liabilities = totalLiability.innerHTML || 0;
+
+    var totalNetWorth = document.getElementById("net-worth");
+    totalNetWorth.innerHTML = parseFloat(assets) - parseFloat(liabilities);
+}
+
