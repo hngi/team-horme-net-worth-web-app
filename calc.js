def52c18192b485c5dda6b087d6611be323ddbe0
Ad3mola <ademolamadelewi@gmail.com>
2019-09-28 15:16:56 +0100

    added

--- calc.js
+++ calc.js
@@ -4,54 +4,72 @@ function currencyChange() {
             "span"
         ).innerHTML = `<span value="" id="currency1">&#8352</span>`;
         document.getElementById("sign").textContent = "NGN";
+        document.getElementById("sign1").textContent = "NGN";
+        document.getElementById("sign2").textContent = "NGN";
     });
     document.getElementById("currency2").addEventListener("click", () => {
         document.getElementById(
             "span"
         ).innerHTML = `<span value="" id="currency2">&#8360</span>`;
-        document.getElementById("sign").textContent = "NGN";
+        document.getElementById("sign").textContent = "RANDS";
+        document.getElementById("sign1").textContent = "RANDS";
+        document.getElementById("sign2").textContent = "RANDS";
     });
     document.getElementById("currency3").addEventListener("click", () => {
         document.getElementById(
             "span"
         ).innerHTML = `<span value="" id="currency3">&#8364</span>`;
         document.getElementById("sign").textContent = "NGN";
+        document.getElementById("sign1").textContent = "NGN";
+        document.getElementById("sign2").textContent = "NGN";
     });
     document.getElementById("currency4").addEventListener("click", () => {
         document.getElementById(
             "span"
         ).innerHTML = `<span value="" id="currency4">&#8371</span>`;
         document.getElementById("sign").textContent = "NGN";
+        document.getElementById("sign1").textContent = "NGN";
+        document.getElementById("sign2").textContent = "NGN";
     });
     document.getElementById("currency5").addEventListener("click", () => {
         document.getElementById(
             "span"
         ).innerHTML = `<span value="" id="currency5">&#8368</span>`;
         document.getElementById("sign").textContent = "NGN";
+        document.getElementById("sign1").textContent = "NGN";
+        document.getElementById("sign2").textContent = "NGN";
     });
     document.getElementById("currency6").addEventListener("click", () => {
         document.getElementById(
             "span"
         ).innerHTML = `<span value="" id="currency6">&#8367</span>`;
         document.getElementById("sign").textContent = "NGN";
+        document.getElementById("sign1").textContent = "NGN";
+        document.getElementById("sign2").textContent = "NGN";
     });
     document.getElementById("currency7").addEventListener("click", () => {
         document.getElementById(
             "span"
         ).innerHTML = `<span value="" id="currency7">&#8366</span>`;
         document.getElementById("sign").textContent = "NGN";
+        document.getElementById("sign1").textContent = "NGN";
+        document.getElementById("sign2").textContent = "NGN";
     });
     document.getElementById("currency8").addEventListener("click", () => {
         document.getElementById(
             "span"
         ).innerHTML = `<span value="" id="currency8">&#8365</span>`;
         document.getElementById("sign").textContent = "NGN";
+        document.getElementById("sign1").textContent = "NGN";
+        document.getElementById("sign2").textContent = "NGN";
     });
     document.getElementById("currency9").addEventListener("click", () => {
         document.getElementById(
             "span"
         ).innerHTML = `<span value="" id="currency9">&#8358</span>`;
         document.getElementById("sign").textContent = "NGN";
+        document.getElementById("sign1").textContent = "NGN";
+        document.getElementById("sign2").textContent = "NGN";
     });
 }
 currencyChange();
