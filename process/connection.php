d1e523b52f425490dc6c026c65510af97709b894
mohammed <ibrahimdamy@gmail.com>
2019-10-04 11:44:57 +0100

    port

--- process/connection.php
+++ process/connection.php
@@ -4,9 +4,8 @@
 	$user		 = 'wfhmuqso';
 	$password	 = 'sLorwsG5twKIkDkH3hu8qpYzD2iP18c8';
 	$database	 = 'wfhmuqso';
-	$port        = 5432
 
-	$conn		 = mysqli_connect($server, $user, $password, $database, $port);
+	$conn		 = mysqli_connect($server, $user, $password, $database);
 
 	if(!$conn){
 		die("connection to this database failed: " .mysqli_connect_error());
