0857227a16579be2168e80df4701d6ea920e633e
Folajin Tobi <tobifolajin@gmail.com>
2019-10-04 20:17:05 +0100

    Update connection.php

--- process/connection.php
+++ process/connection.php
@@ -14,9 +14,10 @@
 
  $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-echo $url;
+
 
   $server = $url["host"];
+echo $server;
   $username = $url["user"];
   $password = $url["pass"];
   $db = substr($url["path"],1);
