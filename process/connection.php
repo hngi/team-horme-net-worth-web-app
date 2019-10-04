4dc6ac44f2516b6599ed4a6050a453a0a82bb7a4
mohammed <ibrahimdamy@gmail.com>
2019-10-04 11:43:08 +0100

    port

--- process/connection.php
+++ process/connection.php
@@ -1,11 +1,12 @@
 
 <?php
-	$server		 = 'dumbo.db.elephantsql.com';
+	$server		 = 'dumbo.db.elephantsql.com (dumbo-01)';
 	$user		 = 'wfhmuqso';
 	$password	 = 'sLorwsG5twKIkDkH3hu8qpYzD2iP18c8';
-	$database		 = 'wfhmuqso';
+	$database	 = 'wfhmuqso';
+	$port        = 5432
 
-	$conn		 = mysqli_connect($server, $user, $password, $database);
+	$conn		 = mysqli_connect($server, $user, $password, $database, $port);
 
 	if(!$conn){
 		die("connection to this database failed: " .mysqli_connect_error());
