fe42422a7b369317e37e91d0697917965307e6ec
Folajin Tobi <tobifolajin@gmail.com>
2019-10-04 19:48:15 +0100

    Update connection.php

--- process/connection.php
+++ process/connection.php
@@ -1,14 +1,25 @@
 <?php
-	$server		 = 'localhost';
-	$user		 = 'root';
-	$password	 = 'root';
-	$db			 = 'networth';
+	//$server		 = 'localhost';
+	//$user		 = 'root';
+	//$password	 = 'root';
+	//$db			 = 'networth';
 
-	$conn		 = mysqli_connect($server, $user, $password, $db);
+	//$conn		 = mysqli_connect($server, $user, $password, $db);
 
-	if(!$conn){
-		die("connection to this database failed: " .mysqli_connect_error());
-	}
+	//if(!$conn){
+		//die("connection to this database failed: " .mysqli_connect_error());
+	//}
 
 	//echo "we connected successfully";
+
+ $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"],1);
+
+  mysqli_connect($server, $username, $password, $db);
+
+  mysql_select_db($db);
 ?>
