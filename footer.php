a7109c0b7ba0fcc273b062b3e17c01fc12845564
darellwest <ewere.njoagwu@gmail.com>
2019-10-06 09:56:42 +0100

    footer removed

---
+++ footer.php
@@ -0,0 +1,86 @@
+<style>
+    /* footer section start here */
+    .footer-section {
+    background: rgb(38, 155, 120);
+   
+
+    }
+
+    .my-list a,
+    .footer-copyright a{
+    text-decoration: none;
+    color: #fff;
+    }
+    .footer-copyright a:hover {
+    text-decoration: underline !important; 
+
+    }
+            
+</style>
+
+<section class="footer-section clearfix">
+        <div class="container">
+            <!-- Footer -->
+            <footer class="page-footer font-small blue pt-4">
+                <!-- Footer Links -->
+                <div class="container-fluid text-center text-md-left clearfix">
+                    <!-- Grid row -->
+                    <div class="row">
+                        <!-- Grid column -->
+                        <div class="col-md-3 mt-md-0 mt-3">
+
+                            <!-- Content -->
+                            <ul class="list-unstyled text-cap ">
+                                <li class="my-list">
+                                    <a href="#!" class="title-color">About Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- Grid column -->
+                        <div class="col-md-3 mt-md-0 mt-3">
+
+                            <!-- Content -->
+                            <ul class="list-unstyled text-cap ">
+                                
+                                <li class="my-list">
+                                    <a href="#!" class="title-color">Contact Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- Grid column -->
+                        <div class="col-md-3 mb-md-0 mb-3">
+
+                            <ul class="list-unstyled text-cap ">
+                                
+                                <li class="my-list">
+                                    <a href="#!" class="title-color">Faq</a>
+                                </li>
+                            </ul>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 mb-md-0 mb-3">
+
+                        <ul class="list-unstyled text-cap ">
+                                
+                            <li class="my-list">
+                                <a href="#!" class="title-color">Subscribe</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- Grid column -->
+
+                </div>
+                <!-- Grid row -->
+                <!-- Copyright -->
+                <div class="footer-copyright text-center py-3">© 2019 Copyright:
+                    <a href="www.hngteamhorme.diaticine.com" class="title-color">
+                        <i class="fa fa-envelope-o"></i>
+                        contact@hngteamhorme
+                    </a>
+                </div>
+                <!-- Copyright -->
+            </footer>
+        </div>
+    </section>
